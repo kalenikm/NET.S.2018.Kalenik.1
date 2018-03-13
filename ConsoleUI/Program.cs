80d64ad3123ce52f280a703be6d00fc139a6093b
kalenikm <kalenikmisha@gmail.com>
2018-03-13 19:02:29 +0300

    QuickSort add

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Logic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = {3, 0, 1, 8, 7, 2, 5, 4, 9, 6};
+            ArraySorter.QuickSort(arr);
+
+            foreach (var num in arr)
+                Console.Write(num + " ");
+        }
+    }
+}
---
+++ Logic/ArraySorter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Logic
+{
+    public static class ArraySorter
+    {
+        public static void QuickSort(int[] array)
+        {
+            if(array == null)
+                throw new ArgumentNullException($"Array {nameof(array)} is null.");
+
+            if(array.Length <= 1)
+                return;
+
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSort(int[] array, int left, int right)
+        {
+            if (right - left < 1)
+                return;
+
+            int pivot = right;
+            int wall = left;
+
+            for (int i = left; i < pivot; i++)
+            {
+                if (array[pivot] > array[i])
+                {
+                    Swap(ref array[i], ref array[wall]);
+                    wall++;
+                }
+            }
+            Swap(ref array[pivot], ref array[wall]);
+
+            if (wall + 1 < right)
+                QuickSort(array, wall + 1, right);
+            if (wall - 1 > left)
+                QuickSort(array, left, wall - 1);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int buff = a;
+            a = b;
+            b = buff;
+        }
+    }
+}
